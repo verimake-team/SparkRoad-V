5776f8d701150be7b57922df24e6b3519a98c883
verimake <zhuyanxiang@verimake.com>
2022-11-01 11:11:47 +0800

    del ov5640 for td5.6

--- demo_v_5_6/15_ov5640_sdram/ov5640_sdram_Runs/syn_1/.opt_gate.begin.f
+++
@@ -1,5 +0,0 @@
-<?xml version="1.0"?>
-<ProcessHandle Version="1" Minor="0">
-    <Process Ownner="zhang" Host="DESKTOP-DRS13TT" Pid="25480">
-    </Process>
-</ProcessHandle>
--- demo_v_5_6/15_ov5640_sdram/ov5640_sdram_Runs/syn_1/.read_design.begin.f
+++
@@ -1,5 +0,0 @@
-<?xml version="1.0"?>
-<ProcessHandle Version="1" Minor="0">
-    <Process Ownner="zhang" Host="DESKTOP-DRS13TT" Pid="6836">
-    </Process>
-</ProcessHandle>
